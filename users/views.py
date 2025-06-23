54673b2906e2125fcfe32cb59a833841e183c3f8
Ronitbhandari <ronitbhandari54321@gmail.com>
2025-06-23 19:54:11 +1000

    User sign up and login APIs

--- users/views.py
+++ users/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import generics
+from .models import CustomUser
+from .serializers import RegisterSerializer
+from rest_framework.permissions import AllowAny
+
+class RegisterView(generics.CreateAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = RegisterSerializer
+    permission_classes = [AllowAny]
