2ee925c4be5aca1a22b6120dd3ab6ef161f66605
Ronitbhandari <ronitbhandari54321@gmail.com>
2025-06-25 22:33:55 +1000

    change in settings file for deployment

--- classroom_portal/settings.py
+++ classroom_portal/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
+
+DATABASES = {
+    'default': dj_database_url.config(default=os.getenv("DATABASE_URL"))
+}
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
